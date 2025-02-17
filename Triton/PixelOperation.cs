15c2197550978ddc1daa9f59a7051ac045d110f3
Ada <yretenai@gmail.com>
2025-02-17 02:36:47 +0000

    pixelops: add SVG blend modes

--- Triton/PixelOperation.cs
+++ Triton/PixelOperation.cs
@@ -4,7 +4,29 @@
 
 namespace Triton;
 
+// https://www.w3.org/TR/SVGCompositing/
 public enum PixelOperation {
 	Copy,
 	AlphaBlend,
+	SrcOver,
+	DstOver,
+	SrcIn,
+	DstIn,
+	SrcOut,
+	DstOut,
+	SrcAtop,
+	DstAtop,
+	Xor,
+	Plus,
+	Multiply,
+	Screen,
+	Overlay,
+	Darken,
+	Lighten,
+	ColorDodge,
+	ColorBurn,
+	HardLight,
+	SoftLight,
+	Difference,
+	Exclusion,
 }
