471a0a1e8fb93796f139f8491ac48a2b391d1b0a
Ada <yretenai@gmail.com>
2025-03-11 16:16:26 +0000

    point: add flipped division operator, also add casting methods

--- Triton/Point.cs
+++ Triton/Point.cs
@@ -22,19 +22,24 @@ public readonly record struct Point<T>(T X, T Y) :
 	IDecrementOperators<Point<T>>,
 	IMinMaxValue<Point<T>>
 	where T : INumber<T>, IMinMaxValue<T> {
-	public static Point<T> operator +(Point<T> left, Point<T> right) => new(left.X + right.X, left.Y + right.Y);
-	public static Point<T> operator -(Point<T> left, Point<T> right) => new(left.X - right.X, left.Y - right.Y);
-	public static Point<T> operator /(Point<T> left, Point<T> right) => new(left.X / right.X, left.Y / right.Y);
-	public static Point<T> operator *(Point<T> left, Point<T> right) => new(left.X * right.X, left.Y * right.Y);
-	static Point<T> IUnaryNegationOperators<Point<T>, Point<T>>.operator -(Point<T> value) => new(-value.X, -value.Y);
-	static Point<T> IUnaryPlusOperators<Point<T>, Point<T>>.operator +(Point<T> value) => new(+value.X, +value.Y);
-	public static Point<T> operator ++(Point<T> value) => new(value.X + T.One, value.Y + T.One);
-	public static Point<T> operator --(Point<T> value) => new(value.X - T.One, value.Y - T.One);
-	public static Point<T> operator +(Point<T> left, T right) => new(left.X + right, left.Y + right);
-	public static Point<T> operator -(Point<T> left, T right) => new(left.X - right, left.Y - right);
-	public static Point<T> operator /(Point<T> left, T right) => new(left.X / right, left.Y / right);
-	public static Point<T> operator *(Point<T> left, T right) => new(left.X * right, left.Y * right);
 	public static Point<T> MaxValue { get; } = new(T.MaxValue, T.MaxValue);
 	public static Point<T> MinValue { get; } = new(T.MinValue, T.MinValue);
 	public static Point<T> Zero { get; } = new(T.Zero, T.Zero);
+	public static Point<T> operator +(Point<T> left, Point<T> right) => new(left.X + right.X, left.Y + right.Y);
+	public static Point<T> operator +(Point<T> left, T right) => new(left.X + right, left.Y + right);
+	public static Point<T> operator -(Point<T> left, Point<T> right) => new(left.X - right.X, left.Y - right.Y);
+	public static Point<T> operator -(Point<T> left, T right) => new(left.X - right, left.Y - right);
+	public static Point<T> operator /(Point<T> left, Point<T> right) => new(left.X / right.X, left.Y / right.Y);
+	public static Point<T> operator /(Point<T> left, T right) => new(left.X / right, left.Y / right);
+	public static Point<T> operator /(T left, Point<T> right) => new(left / right.X, left / right.Y);
+	public static Point<T> operator *(Point<T> left, Point<T> right) => new(left.X * right.X, left.Y * right.Y);
+	public static Point<T> operator *(Point<T> left, T right) => new(left.X * right, left.Y * right);
+	public static Point<T> operator ++(Point<T> value) => new(value.X + T.One, value.Y + T.One);
+	public static Point<T> operator --(Point<T> value) => new(value.X - T.One, value.Y - T.One);
+	static Point<T> IUnaryNegationOperators<Point<T>, Point<T>>.operator -(Point<T> value) => new(-value.X, -value.Y);
+	static Point<T> IUnaryPlusOperators<Point<T>, Point<T>>.operator +(Point<T> value) => new(+value.X, +value.Y);
+	public Point<TOther> As<TOther>() where TOther : INumber<TOther>, IMinMaxValue<TOther> => CastTruncating<TOther>();
+	public Point<TOther> CastChecked<TOther>() where TOther : INumber<TOther>, IMinMaxValue<TOther> => new(TOther.CreateChecked(X), TOther.CreateChecked(Y));
+	public Point<TOther> CastSaturating<TOther>() where TOther : INumber<TOther>, IMinMaxValue<TOther> => new(TOther.CreateSaturating(X), TOther.CreateSaturating(Y));
+	public Point<TOther> CastTruncating<TOther>() where TOther : INumber<TOther>, IMinMaxValue<TOther> => new(TOther.CreateTruncating(X), TOther.CreateTruncating(Y));
 }
