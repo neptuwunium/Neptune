247364b44cacc17d7ae2eb2544b033de9dd3f590
Ada <yretenai@gmail.com>
2025-03-26 15:16:16 +0000

    repo: dual license EUPL-1.2 and LGPL-3.0+

--- Triton/SamplingOperations.cs
+++ Triton/SamplingOperations.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2025 Legiayayana
 //
-// SPDX-License-Identifier: EUPL-1.2
+// SPDX-License-Identifier: EUPL-1.2 OR LGPL-3.0-or-later
+// You may choose either license when using or modifying this code.
 
 using System.Numerics;
 using Triton.Pixel;
