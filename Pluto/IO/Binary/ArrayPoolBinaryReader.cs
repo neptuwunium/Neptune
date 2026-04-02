73271894b0bdd809db672b9d77b849be93552bdc
Ada Freya Ahmed <ada@chronovore.dev>
2026-04-02 09:22:22 +0530

    chore: simplify ArrayPoolBinaryReader

--- Pluto/IO/Binary/ArrayPoolBinaryReader.cs
+++ Pluto/IO/Binary/ArrayPoolBinaryReader.cs
@@ -7,44 +7,46 @@ using System.Runtime.CompilerServices;
 
 namespace Pluto.IO.Binary;
 
-public class ArrayPoolBinaryReader : ArrayBinaryReader {
-	public ArrayPoolBinaryReader(byte[] array, int length, bool leaveOpen = false) : base(array) {
+public class ArrayPoolBinaryReader : BufferBinaryReader {
+	public ArrayPoolBinaryReader(byte[] array, int length, bool leaveOpen = false) {
 		LeaveOpen = leaveOpen;
 		Length = length;
+		Array = array;
+		Rented = new RentedArray<byte>(array, length);
 	}
 
-	public ArrayPoolBinaryReader(RentedArray<byte> array, bool leaveOpen = false) : base(array.Array) {
+	public ArrayPoolBinaryReader(IRentedArray<byte> array, bool leaveOpen = false) {
 		LeaveOpen = leaveOpen;
 		Length = array.Length;
 		Rented = array;
 	}
 
+	public override int Position { get; set; }
 	public override int Length { get; }
-	protected RentedArray<byte>? Rented { get; }
+	protected byte[]? Array { get; set; }
+	protected IRentedArray<byte> Rented { get; set; }
+
+	public override void ReadBytes(Span<byte> span) {
+		if (Array is { } array) {
+			array.AsSpan(Position, Length).CopyTo(span);
+		} else {
+			Rented.Span.Slice(Position, span.Length).CopyTo(span);
+		}
+		
+		Position += span.Length;
+	}
 
 	public bool LeaveOpen { get; }
 
 	public override IRentedArray<T> ReadShared<T>(int length) where T : struct {
-		if (Rented == null) {
-			return new RentedArray<T>(0);
-		}
-
 		var arr = new UnownedCovariantArray<T>(Rented, Position, length);
-
 		Position += Unsafe.SizeOf<T>() * length;
-
 		return arr;
 	}
 
 	public override IRentedArray<byte> ReadSharedBytes(int length) {
-		if (Rented == null) {
-			return new RentedArray<byte>(0);
-		}
-
 		var arr = new UnownedRentedArray<byte>(Rented, Position, length);
-
 		Position += length;
-
 		return arr;
 	}
 
@@ -53,11 +55,6 @@ public class ArrayPoolBinaryReader : ArrayBinaryReader {
 			return;
 		}
 
-		if (Rented != null) {
-			Rented.Dispose();
-			return;
-		}
-
-		ArrayPool<byte>.Shared.Return(Array);
+		Rented.Dispose();
 	}
 }
