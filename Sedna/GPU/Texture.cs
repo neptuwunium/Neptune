733b65fc3b4355857057ecdca2372aa3c2be7d3d
Ada Freya Ahmed <ada@chronovore.dev>
2026-04-19 12:42:01 +0530

    sedna[texture]: implement Create

--- Sedna/GPU/Texture.cs
+++ Sedna/GPU/Texture.cs
@@ -2,7 +2,6 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
-using System.Buffers;
 using Pluto.IO.Binary;
 using SDL;
 using static SDL.SDL3;
@@ -17,20 +16,85 @@ public class Texture : ManagedResource<TextureResourceId> {
 	public SDL_GPUTextureCreateInfo TextureInfo { get; set; } = new();
 	public SDL_GPUSamplerCreateInfo SamplerInfo { get; set; } = new();
 
-	public MemoryHandle TextureHandle { get; set; }
 	public unsafe SDL_GPUSampler* DeviceSampler { get; set; }
 	public unsafe SDL_GPUTexture* DeviceTexture { get; set; }
 
 	public override unsafe void Create() {
-		throw new NotImplementedException();
+		var device = Manager.Scene.Renderer.DeviceHandle;
+
+		if (DeviceTexture != null) {
+			if (Data == null) {
+				// todo logging
+				return;
+			}
+
+			var info = TextureInfo;
+			DeviceTexture = SDL_CreateGPUTexture(device, &info);
+			if (DeviceTexture == null) {
+				// todo logging
+				return;
+			}
+
+			var transferInfo = new SDL_GPUTransferBufferCreateInfo {
+				usage = SDL_GPUTransferBufferUsage.SDL_GPU_TRANSFERBUFFERUSAGE_UPLOAD,
+				size = (uint) Data.Length,
+			};
+
+			var transfer = SDL_CreateGPUTransferBuffer(device, &transferInfo);
+			if (transfer == null) {
+				Destroy();
+				return;
+			}
+
+			var map = SDL_MapGPUTransferBuffer(device, transfer, false);
+			if (map == nint.Zero) {
+				SDL_ReleaseGPUTransferBuffer(device, transfer);
+				Destroy();
+				return;
+			}
+
+			using (var pinned = Data.Memory.Pin()) {
+				Buffer.MemoryCopy(pinned.Pointer, (void*) map, Data.Length, Data.Length);
+			}
+
+			SDL_UnmapGPUTransferBuffer(device, transfer);
+
+			var cmd = SDL_AcquireGPUCommandBuffer(device);
+			var pass = SDL_BeginGPUCopyPass(cmd);
+
+			var src = new SDL_GPUTextureTransferInfo {
+				transfer_buffer = transfer,
+				offset = 0, // todo: layers, mips
+				pixels_per_row = 0, // tightly packed, defaults to width 
+				rows_per_layer = 0, // tightly packed, defaults to height
+			};
+
+			var dst = new SDL_GPUTextureRegion {
+				texture = DeviceTexture,
+				w = TextureInfo.width,
+				h = TextureInfo.height,
+				d = TextureInfo.layer_count_or_depth,
+				layer = 0, // todo: layers
+				mip_level = 0, // todo: mips
+				x = 0,
+				y = 0,
+				z = 0,
+			};
+
+			SDL_UploadToGPUTexture(pass, &src, &dst, false);
+
+			SDL_EndGPUCopyPass(pass);
+			SDL_SubmitGPUCommandBuffer(cmd);
+			SDL_ReleaseGPUTransferBuffer(device, transfer);
+		}
+
+		if (DeviceSampler != null) {
+			var info = SamplerInfo;
+			DeviceSampler = SDL_CreateGPUSampler(device, &info);
+		}
 	}
 
 	public override unsafe void Destroy() {
-		if (TextureHandle.Pointer != null) {
-			TextureHandle.Dispose();
-			TextureHandle = default;
-		}
-		
 		if (DeviceSampler != null) {
 			SDL_ReleaseGPUSampler(Manager.Scene.Renderer.DeviceHandle, DeviceSampler);
 			DeviceSampler = null;
