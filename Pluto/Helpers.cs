fc98ff030eeda4fd02efb65d48effcd78699bae9
Ada Freya Ahmed <ada@chronovore.dev>
2026-05-16 12:27:40 +0530

    pluto[sourcegen]: reverse endianness now supports most generics

--- Pluto/Helpers.cs
+++ Pluto/Helpers.cs
@@ -2,6 +2,9 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
 namespace Pluto;
 
 public static class Helpers {
@@ -12,4 +15,28 @@ public static class Helpers {
 					CultureInfo.CurrentUICulture =
 						CultureInfo.DefaultThreadCurrentCulture =
 							CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+
+	public static T ReverseStruct<T, TInner>(T value) where T : unmanaged {
+		var elementSize = Unsafe.SizeOf<TInner>();
+		if (elementSize <= 1) {
+			return value;
+		}
+
+		var reversed = value;
+		var bytes = MemoryMarshal.AsBytes(new Span<T>(ref reversed));
+		if (bytes.Length == elementSize) {
+			bytes.Reverse();
+			return reversed;
+		}
+
+		if (bytes.Length % elementSize != 0) {
+			throw new InvalidOperationException("struct is not a multiple!");
+		}
+
+		for (var i = 0; i < bytes.Length; i += elementSize) {
+			bytes.Slice(i, elementSize).Reverse();
+		}
+
+		return reversed;
+	}
 }
