7c22378b24940174adbe5a067ecd4f6182f8e8ae
Ada Freya Ahmed <ada@chronovore.dev>
2026-05-17 19:21:43 +0530

    triton: ColorABGR

---
+++ Triton/Pixel/Formats/ColorABGR.cs
@@ -0,0 +1,37 @@
+// SPDX-FileCopyrightText: 2026 Neptuwunium
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+using System.Numerics;
+
+namespace Triton.Pixel.Formats;
+
+public record struct ColorABGR<T>(T A, T B, T G, T R) : IColor<ColorABGR<T>, T> where T : unmanaged, INumberBase<T>, IMinMaxValue<T> {
+	public readonly void GetChannels(out T r, out T g, out T b, out T a) {
+		r = R;
+		g = G;
+		b = B;
+		a = A;
+	}
+
+	public void SetChannels(T r, T g, T b, T a) {
+		R = r;
+		G = g;
+		B = b;
+		A = a;
+	}
+
+	static bool IColor.HasRedChannel => true;
+	static bool IColor.HasGreenChannel => true;
+	static bool IColor.HasBlueChannel => true;
+	static bool IColor.HasAlphaChannel => true;
+	static bool IColor.ChannelsAreFullyUtilized => true;
+	static Type IColor.ChannelType => typeof(T);
+	static ChannelLayout IColor.ChannelLayout => ChannelLayout.BlueFirst;
+
+	public static ColorABGR<T> Black => new(NumericConversion.GetMinimumValueSafe<T>(), NumericConversion.GetMinimumValueSafe<T>(), NumericConversion.GetMinimumValueSafe<T>(), NumericConversion.GetMaximumValueSafe<T>());
+	public static ColorABGR<T> White => new(NumericConversion.GetMaximumValueSafe<T>(), NumericConversion.GetMaximumValueSafe<T>(), NumericConversion.GetMaximumValueSafe<T>(), NumericConversion.GetMaximumValueSafe<T>());
+	public static ColorABGR<T> Transparent => Black with { A = NumericConversion.GetMinimumValueSafe<T>() };
+
+	public override string ToString() => $"{{ A: {A}, R: {R}, G: {G}, B: {B} }}";
+}
